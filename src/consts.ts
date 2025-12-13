e741f8012ecbac60e54e2b47ce80689974adbff7
LTSXx <139348037+class-bin-data@users.noreply.github.com>
2025-12-13 22:25:13 +0800

    Update consts.ts

--- src/consts.ts
+++ src/consts.ts
@@ -8,9 +8,9 @@ export const BLOG_SLUG_KEY = process.env.BLOG_SLUG_KEY || ''
  * GitHub 仓库配置
  */
 export const GITHUB_CONFIG = {
-	OWNER: process.env.NEXT_PUBLIC_GITHUB_OWNER || '2465321',
-	REPO: process.env.NEXT_PUBLIC_GITHUB_REPO || '1c87378e92d3a0b1b21e621ab84df45b5c0b0568',
+	OWNER: process.env.NEXT_PUBLIC_GITHUB_OWNER || 'LTSXx',
+	REPO: process.env.NEXT_PUBLIC_GITHUB_REPO || '2025-blog-public',
 	BRANCH: process.env.NEXT_PUBLIC_GITHUB_BRANCH || 'main',
-	APP_ID: process.env.NEXT_PUBLIC_GITHUB_APP_ID || '-',
-	ENCRYPT_KEY: process.env.NEXT_PUBLIC_GITHUB_ENCRYPT_KEY || 'woshiguanliyuan8907',
+	APP_ID: process.env.NEXT_PUBLIC_GITHUB_APP_ID || '2465321',
+	ENCRYPT_KEY: process.env.NEXT_PUBLIC_GITHUB_ENCRYPT_KEY || '1c87378e92d3a0b1b21e621ab84df45b5c0b0568',
 } as const
